8f2b86dd0aa88ad5c07a76eac7b222efd6a25b8b
Jeff Kilroy <jeffkilroy@gmail.com>
2017-10-03 13:35:32 -0400

    Create _ReportClassName_ReportController.php

---
+++ app/Http/Controllers/Reports/_ReportClassName_ReportController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Reports;
+
+use App\Http\Controllers\Reports\BaseReportController;
+
+class _ReportClassName_Controller extends BaseReportController
+{
+
+    protected $reportClass = \App\Reports\_ReportClassName_Report::class;
+    protected $name = '_ReportClassName_';
+
+}
